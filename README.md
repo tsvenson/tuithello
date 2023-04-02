3f53d07e50280fcb2c252537b99c1b164ec947f3
Thomas Svensson <thomas@tsvenson.com>
2023-04-02 20:47:31 +0200

    Can be installed with pip, runs with 'tuithello' and shows a basic TUI-layout.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tuithello an Othello game in the terminal
+
+Written in Python using Textual
+
+This is simply a learning project, where creating a game makes learning som uch more funs.
+
+## Built using:
+
+- [Python 3.11](https://www.python.org/downloads/)
+- [Textual](https://github.com/Textualize/textual)
+
+## Installation
+
+Run `pip install .` to install.
+
+## Run
+
+Run `tuithello` to run the game.
---
+++ src/tuithello/main.py
@@ -0,0 +1,42 @@
+"""
+Tuithello is an Othello game that runs in the terminal.
+"""
+from rich.text import Text
+from rich.emoji import Emoji
+
+from textual.app import App, ComposeResult
+from textual.containers import Container
+from textual.widget import Widget
+from textual.widgets import Header, Footer, Label
+
+
+class TuithelloApp(App):
+    """The Tuithello app to run the game."""
+    CSS_PATH = "tuithello.css"
+
+    TITLE = "Tuithello"
+
+    BINDINGS = [
+        ("ctrl+d", "toggle_dark", "Toggle dark mode"),
+        ("ctrl+q", "quit", "Quite the game")
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Container(Label(Text(Emoji.replace('tuithello: :white_circle: :black_circle:'))), id="board")
+        yield Footer()
+    
+    def action_toggle_dark(self) -> None:
+        self.dark = not self.dark
+
+    def action_quit(self) -> None:
+        self.app.exit()
+
+
+def app():
+    game = TuithelloApp()
+    game.run()
+
+
+if __name__ == "__main__":
+    app()
